2b853dccc55946823f7b92d5ebc791502df4cc99
yangxiufeng <yangxiufeng@douban.com>
2015-10-14 11:50:40 +0800

    manage htree

---
+++ src/store/htree.go
@@ -0,0 +1,287 @@
+package store
+
+import (
+	"bytes"
+	"fmt"
+	"sync"
+)
+
+const (
+	BUCKET_SIZE = 16
+	MAX_DEPTH   = 8
+)
+
+var KHASH_LENS = [8]int{8, 8, 7, 7, 6, 6, 5, 5}
+
+type HTree struct {
+	sync.Mutex
+	// args
+	depth int
+	pos   int
+
+	// runtime
+	levels  [][]Node
+	leafs []bytesLeaf
+	maxLeaf int
+
+	// tmp, to avoid alloc
+	ni NodeInfo
+}
+
+type Node struct {
+	count   uint32
+	// size    uint32 //including deleted
+	hash    uint16
+	isValid bool
+	c_count uint8  // TODO: remove later
+}
+
+type NodeInfo struct {
+	node   *Node
+	level  int
+	offset int
+	path   []int
+}
+
+func itemHash(h uint64, m *HTreeItem) uint16 {
+	if m.ver < 0 {
+		return 0
+	}
+	return m.vhash * uint16(h>>32) // TODO: test is
+}
+
+func newHTree(depth, pos, height int) *HTree {
+	if depth+height > MAX_DEPTH {
+		panic("HTree too high")
+	}
+	tree := new(HTree)
+	tree.depth = depth
+	tree.pos = pos
+	tree.levels = make([][]Node, height)
+	size := 1
+	for i := 0; i < height; i++ {
+		tree.levels[i] = make([]Node, size)
+		size *= 16
+	}
+	size /= 16
+	leafnodes := tree.levels[height-1]
+	for i := 0; i < size; i++ {
+		leafnodes[i].isValid = true
+	}
+	tree.leafs = make([]bytesLeaf, size)
+	return tree
+}
+
+func (tree *HTree) getHex(khash uint64, level int) int {
+	depth := level + tree.depth
+	shift := (16 - depth - 1) * 4
+	return int(0xf & (khash >> uint32(shift)))
+}
+
+func (tree *HTree) setLeaf(req *HTreeReq, ni *NodeInfo) {
+	node := ni.node
+	leaf := tree.leafs[ni.offset]
+	oldm, ok, newleaf := leaf.Set(req, ni)
+	tree.leafs[ni.offset] = newleaf
+	if req.item.ver > 0 {
+		node.hash += itemHash(req.item.keyhash, &req.item)
+		node.count += 1
+	}
+	if ok && oldm.ver > 0 {
+		node.hash += itemHash(req.item.keyhash, &oldm)
+		node.count -= 1
+	}
+}
+
+func (tree *HTree) getLeaf(ki *KeyInfo, ni *NodeInfo) {
+	ni.level = len(tree.levels) - 1
+	ni.offset = 0
+	ni.path = ki.KeyPath[tree.depth:]
+	for level := 1; level < len(tree.levels); level += 1 {
+		ni.offset = ni.offset*16 + ni.path[level-1]
+	}
+	ni.node = &tree.levels[ni.level][ni.offset]
+	ni.path = ki.KeyPath[:tree.depth+ni.level]
+	return
+}
+
+func (tree *HTree) getLeafAndInvalidNodes(ki *KeyInfo, ni *NodeInfo) {
+	ni.level = len(tree.levels) - 1
+
+	ni.offset = 0
+	ni.path = ki.KeyPath[tree.depth:]
+	tree.levels[0][0].isValid = false
+	for level := 1; level < len(tree.levels)-1; level += 1 {
+		ni.offset = ni.offset*16 + ni.path[level-1]
+		tree.levels[level][ni.offset].isValid = false
+	}
+	ni.offset = ni.offset*16 + ni.path[ni.level-1]
+	ni.node = &tree.levels[ni.level][ni.offset]
+	ni.path = ki.KeyPath[:tree.depth+ni.level]
+	return
+}
+
+func (tree *HTree) getNode(ki *KeyInfo, ni *NodeInfo) {
+	offset := 0
+
+	l := tree.depth + len(tree.levels) - 1
+	if len(ki.KeyPath) < l {
+		l = len(ki.KeyPath)
+	}
+	h := tree.depth + 1
+	for ; h < l; h += 1 {
+		offset = offset*16 + ki.KeyPath[h-1]
+	}
+	ni.level = h - tree.depth
+	if ni.level > len(tree.levels)-1 {
+		panic(fmt.Sprintf("level TOO LARGE %d l=%d", h, l))
+	}
+	ni.offset = offset
+	ni.node = &tree.levels[ni.level][ni.offset]
+	ni.path = ki.KeyPath[:tree.depth+ni.level]
+	return
+}
+
+func (tree *HTree) set(ki *KeyInfo, meta *Meta, pos Position) {
+	var req HTreeReq
+	req.ki = ki
+	req.Meta = *meta
+	req.Position = pos
+	req.item = HTreeItem{ki.KeyHash, pos.encode(), meta.Ver, meta.ValueHash}
+	tree.setReq(&req)
+}
+
+func (tree *HTree) setReq(req *HTreeReq) {
+	tree.Lock()
+	defer tree.Unlock()
+
+	tree.getLeafAndInvalidNodes(req.ki, &tree.ni)
+	tree.setLeaf(req, &tree.ni)
+	if int(tree.ni.node.count) > tree.maxLeaf {
+		tree.maxLeaf = int(tree.ni.node.count)
+	}
+}
+
+func (tree *HTree) get(ki *KeyInfo) (meta *Meta, pos Position, found bool) {
+	var req HTreeReq
+	req.ki = ki
+	found = tree.getReq(&req)
+	meta = &Meta{0, 0, req.item.ver, req.item.vhash}
+	pos = decodePos(req.item.pos)
+	return
+}
+
+func (tree *HTree) getReq(req *HTreeReq) (found bool) {
+	tree.Lock()
+	defer tree.Unlock()
+	ni := &tree.ni
+	tree.getLeaf(req.ki, ni)
+
+	found = tree.leafs[ni.offset].Get(req, ni)
+	return
+}
+
+func (tree *HTree) updateNodes(level, offset int) (node *Node) {
+	node = &tree.levels[level][offset]
+	if node.isValid {
+		return
+	}
+	node.count = 0
+	var hashs [16]uint16
+	for i := 0; i < 16; i++ {
+		cnode := tree.updateNodes(level+1, offset*16+i)
+		node.count += cnode.count
+		hashs[i] = cnode.hash
+	}
+	node.hash = 0
+	for i := 0; i < 16; i++ {
+		if node.count > htreeConfig.ThresholdBigHash {
+			node.hash *= 97
+		}
+		node.hash += hashs[i]
+	}
+	node.isValid = true
+	return
+}
+
+func (tree *HTree) collectItems(ni *NodeInfo, items []HTreeItem, filterpath []int) []HTreeItem {
+	var pathBuf [16]int
+	if ni.level >= len(tree.levels)-1 {
+		f := func(h uint64, m *HTreeItem) {
+			if filterpath == nil || isSamePath(ParsePathUint64(h, pathBuf[:16]), filterpath, len(filterpath)) {
+				items = append(items, *m)
+			}
+		}
+		tree.leafs[ni.offset].Iter(f, ni)
+	} else {
+		var c NodeInfo
+		c.level = ni.level + 1
+		var cpathBuf [8]int
+		c.path = cpathBuf[:tree.depth+c.level]
+		copy(c.path, ni.path)
+		for i := 0; i < 16; i++ {
+			c.offset = ni.offset*16 + i
+			c.node = &tree.levels[c.level][c.offset]
+			c.path[tree.depth+ni.level] = i
+			items = tree.collectItems(&c, items, filterpath)
+		}
+	}
+	return items
+}
+
+func (tree *HTree) listDir(ki *KeyInfo) (items []HTreeItem, nodes []Node) {
+	var ni NodeInfo
+	if len(ki.KeyPath) == tree.depth {
+		ni.node = &tree.levels[0][0]
+	} else {
+		tree.getNode(ki, &ni)
+	}
+
+	node := ni.node
+	tree.updateNodes(ni.level, ni.offset)
+	if ni.level >= len(tree.levels)-1 || node.count < htreeConfig.ThresholdListKey {
+		// TODO: should use buffer(outside ListDir), since the count is limited
+		items = make([]HTreeItem, 0, node.count) // FIXME: item maybe deleted!
+		filterpath := ki.KeyPath
+		if ni.level+tree.depth == len(ki.Key) {
+			filterpath = nil
+		}
+		items = tree.collectItems(&ni, items, filterpath)
+		return
+	} else {
+		nodes = make([]Node, 16)
+		for i := 0; i < 16; i++ {
+			// TODO: avoid copy
+			nodes[i] = tree.levels[ni.level+1][ni.offset*16+i]
+		}
+	}
+	return
+}
+
+func (tree *HTree) ListDir(ki *KeyInfo) (ret []byte, err error) {
+	if err != nil {
+		return nil, err
+	}
+	if len(ki.Key) < tree.depth {
+		return nil, fmt.Errorf("bad dir path to list: too short")
+	}
+	tree.Lock()
+	defer tree.Unlock()
+
+	items, nodes := tree.listDir(ki)
+	var buffer bytes.Buffer
+	if items != nil {
+		for _, item := range items {
+			s := fmt.Sprintf("%016x %d %d\n", item.keyhash, int(item.vhash), item.ver)
+			buffer.WriteString(s)
+		}
+		return buffer.Bytes(), nil
+	} else if nodes != nil {
+		for i, n := range nodes {
+			s := fmt.Sprintf("%x/ %d %d\n", i, n.hash, int(n.count)+int(n.c_count))
+			buffer.WriteString(s)
+		}
+		return buffer.Bytes(), nil
+	}
+	return nil, nil
+}
---
+++ src/store/leaf.go
@@ -0,0 +1,152 @@
+package store
+
+/*
+#include<stdlib.h>
+#include<string.h>
+int find(void *ss, void *s, int item_size, int cmp_size, int n) {
+	char *p = (char*)ss;
+	int i;
+	for (i = 0; i < n; i++, p += item_size) {
+		if (0 == memcmp(p, s, cmp_size))
+			return i;
+	}
+	return -1;
+}
+*/
+import "C"
+
+import (
+	"bytes"
+	"encoding/binary"
+	"reflect"
+	"unsafe"
+)
+
+const LEN_USE_C_FIND = 100
+
+// BlockArrayLeaf
+
+type bytesLeaf []byte
+
+type ItemFunc func(uint64, *HTreeItem)
+
+func getNodeKhash(path []int) uint32 {
+	var khash uint32
+	for i, off := range path {
+		khash += uint32(((off & 0xf) << uint32((4 * (7 - i)))))
+	}
+	return khash
+}
+
+func bytesToItem(b []byte, item *HTreeItem) {
+	item.pos = binary.LittleEndian.Uint32(b)
+	item.ver = int32(binary.LittleEndian.Uint32(b[4:]))
+	item.vhash = binary.LittleEndian.Uint16(b[8:])
+}
+
+func itemToBytes(b []byte, item *HTreeItem) {
+	binary.LittleEndian.PutUint32(b, item.pos)
+	binary.LittleEndian.PutUint32(b[4:], uint32(item.ver))
+	binary.LittleEndian.PutUint16(b[8:], item.vhash)
+}
+
+func khashToBytes(b []byte, khash uint64) {
+	binary.LittleEndian.PutUint64(b, khash)
+}
+
+func bytesToKhash(b []byte) (khash uint64) {
+	return binary.LittleEndian.Uint64(b)
+}
+
+func (leaf bytesLeaf) find(req *HTreeReq, ni *NodeInfo, lenKHash int) int {
+	size := len(leaf)
+	var khashBytes [8]byte
+	khashToBytes(khashBytes[0:], req.ki.KeyHash)
+	kb := khashBytes[:lenKHash]
+	lenItem := lenKHash + 10
+	n := len(leaf) / lenItem
+	if n < LEN_USE_C_FIND {
+		for i := 0; i < size; i += lenItem {
+			if bytes.Compare(leaf[i:i+lenKHash], kb) == 0 {
+				return i
+			}
+		}
+	} else {
+		ss := (*reflect.SliceHeader)((unsafe.Pointer(&leaf))).Data
+		s := (*reflect.SliceHeader)((unsafe.Pointer(&kb))).Data
+		i := int(C.find((unsafe.Pointer(ss)), unsafe.Pointer(s), C.int(lenItem), C.int(lenKHash), C.int(n)))
+		return i * lenItem
+	}
+	return -1
+}
+
+// not filled with 0!
+func (leaf bytesLeaf) enlarge(size int) []byte {
+	var b []byte
+	src := (*reflect.SliceHeader)((unsafe.Pointer(&leaf)))
+	dst := (*reflect.SliceHeader)((unsafe.Pointer(&b)))
+	if leaf != nil {
+		dst.Data = uintptr(C.realloc(unsafe.Pointer(src.Data), C.size_t(size)))
+	} else {
+		dst.Data = uintptr(C.malloc(C.size_t(size)))
+	}
+	dst.Cap = size
+	dst.Len = size
+	return b
+}
+
+func (leaf bytesLeaf) enlarge2(size int) []byte {
+	tmp := make([]byte, size)
+	copy(tmp, []byte(leaf))
+	return tmp
+}
+
+func (leaf bytesLeaf) Set(req *HTreeReq, ni *NodeInfo) (oldm HTreeItem, exist bool, newLeaf bytesLeaf) {
+	lenKHash := KHASH_LENS[len(ni.path)]
+	idx := leaf.find(req, ni, lenKHash)
+	exist = (idx >= 0)
+	var dst []byte
+	var tmp []byte
+	if exist {
+		bytesToItem(leaf[idx+lenKHash:], &oldm)
+		dst = leaf[idx:]
+		newLeaf = leaf
+	} else {
+		newSize := len(leaf) + lenKHash + 10
+		tmp = leaf.enlarge(newSize)
+		dst = tmp[len(leaf):]
+		newLeaf = tmp
+	}
+	khashToBytes(dst, req.ki.KeyHash)
+	itemToBytes(dst[lenKHash:], &req.item)
+	return
+}
+
+func (leaf bytesLeaf) Get(req *HTreeReq, ni *NodeInfo) (exist bool) {
+	lenKHash := KHASH_LENS[len(ni.path)]
+	idx := leaf.find(req, ni, lenKHash)
+	exist = (idx >= 0)
+	if exist {
+		bytesToItem(leaf[idx+lenKHash:], &req.item)
+	}
+	return
+}
+
+func (leaf bytesLeaf) Iter(f ItemFunc, ni *NodeInfo) {
+	lenKHash := KHASH_LENS[len(ni.path)]
+	size := len(leaf)
+	lenItem := lenKHash + 10
+	var m HTreeItem
+	var khash uint64
+
+	mask := ((uint64(1) << (uint32(lenKHash) * 8)) - 1)
+	nodeKHash := uint64(getNodeKhash(ni.path)) << 32 & (^mask)
+	for i := 0; i < size; i += lenItem {
+		bytesToItem(leaf[i+lenKHash:], &m)
+		khash = bytesToKhash(leaf[i:])
+		khash &= mask
+		khash += nodeKHash
+		f(khash, &m)
+	}
+	return
+}
