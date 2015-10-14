17ea6d7611ecf9523302e67ed045014ce0d1ff8e
yangxiufeng <yangxiufeng@douban.com>
2015-10-14 11:49:13 +0800

    manage data file

---
+++ src/store/data.go
@@ -0,0 +1,214 @@
+package store
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sync"
+	"time"
+)
+
+const (
+	MAX_CHUNK = 256
+)
+
+type dataStore struct {
+	home string
+
+	sync.Mutex
+
+	oldHead int // old tail == 0
+	newHead int
+	newTail int
+
+	currOffset uint32
+
+	filesizes [MAX_CHUNK]uint32
+	wbufs     [MAX_CHUNK][]*record
+	wbufSize  uint32
+}
+
+func NewdataStore(home string) *dataStore {
+	ds := new(dataStore)
+	ds.home = home
+	return ds
+}
+
+func genPath(home string, chunkID int) string {
+	if chunkID > config.maxOldChunk {
+		return fmt.Sprintf("%s/%03d.data", home, chunkID)
+	}
+	// chunkID -= dataConfig.maxOldChunk
+	return fmt.Sprintf("%s/%03d.data", home, chunkID)
+}
+
+func (ds *dataStore) genPath(chunkID int) string {
+
+	return genPath(ds.home, chunkID)
+}
+
+func (ds *dataStore) nextChunkID(chunkID int) int {
+	if chunkID < 255 {
+		return chunkID + 1
+	} else {
+		return config.maxOldChunk
+	}
+}
+
+func (ds *dataStore) AppendRecord(rec *Record) (pos Position, err error) {
+	// TODO: check err of last flush
+	rec.Compress()
+	r := wrapRecord(rec)
+	_, size := recordSize(rec)
+	ds.Lock()
+	if ds.currOffset+size > dataConfig.MaxFileSize {
+		ds.newHead++
+		ds.currOffset = 0
+		logger.Infof("rotate to %d (%d %d %#v)", ds.newHead, ds.currOffset, size, config)
+		go ds.flush(ds.newHead - 1)
+	}
+	pos.ChunkID = ds.newHead
+	pos.Offset = ds.currOffset
+	r.pos = pos
+	ds.wbufs[ds.newHead] = append(ds.wbufs[ds.newHead], r)
+	ds.currOffset += size
+	ds.Unlock()
+	return
+}
+
+func (ds *dataStore) flusher() {
+	for {
+		ds.flush(-1)
+		time.Sleep(1 * time.Minute)
+	}
+}
+
+func (ds *dataStore) flush(chunk int) error {
+	// TODO: need a flush lock
+	ds.Lock()
+	if chunk < 0 {
+		chunk = ds.newHead
+	}
+	n := len(ds.wbufs[chunk])
+	ds.Unlock()
+	// logger.Infof("flushing %d records to data %d", n, chunk)
+
+	w, err := ds.getStreamWriter(chunk, true)
+	if err != nil {
+		return err
+	}
+	for i := 0; i < n; i++ {
+		ds.Lock()
+		r := ds.wbufs[chunk][i]
+		ds.Unlock()
+		w.append(r)
+	}
+	ds.Lock()
+	ds.wbufs[chunk] = ds.wbufs[chunk][n:]
+	ds.Unlock()
+
+	return w.Close()
+}
+
+func (ds *dataStore) GetRecordByPos(pos Position) (res *Record, err error) {
+	res = nil
+	ds.Lock()
+	wbuf := ds.wbufs[pos.ChunkID]
+	n := len(wbuf)
+	if n > 0 && pos.Offset >= wbuf[0].pos.Offset {
+		for _, rec := range wbuf {
+			if rec.pos.Offset == pos.Offset {
+				// TODO: otherwise may need ref count to release []byte?
+				res = rec.rec.Copy()
+			}
+		}
+	}
+	ds.Unlock()
+	if res != nil {
+		return
+	}
+	rec, e := readRecordAtPath(ds.genPath(pos.ChunkID), pos.Offset)
+	if e != nil {
+		return nil, e
+	}
+	rec.rec.Payload.Decompress()
+	return rec.rec, nil
+}
+
+func (ds *dataStore) Truncate(chunk int, size uint32) error {
+	path := ds.genPath(chunk)
+	st, err := os.Stat(path)
+	if err != nil {
+		logger.Infof(err.Error())
+		return err
+	}
+	logger.Infof("truncate %s %d to %d", path, st.Size(), size)
+	return os.Truncate(ds.genPath(chunk), int64(size))
+}
+
+func (ds *dataStore) ListFiles() ([]Position, error) {
+	res := make([]Position, 0, 256)
+	for i := 0; i < 256; i++ {
+		path := genPath(ds.home, i)
+		st, err := os.Stat(path)
+		if err != nil {
+			logger.Infof(err.Error())
+			return nil, err // TODO: need another way to check
+		} else {
+			res = append(res, Position{i, uint32(st.Size())})
+		}
+	}
+	return res, nil
+}
+
+func (ds *dataStore) GetCurrPos() Position {
+	return Position{ds.newHead, ds.currOffset}
+}
+
+func (ds *dataStore) DeleteFile(chunkID int) error {
+	return os.Remove(ds.genPath(chunkID))
+}
+
+func (ds *dataStore) GetStreamReader(chunk int) (*DataStreamReader, error) {
+	path := ds.genPath(chunk)
+	return newDataStreamReader(path, 1<<20)
+}
+
+func (ds *dataStore) GetStreamWriter(chunk int, isappend bool) (*DataStreamWriter, error) {
+	return ds.getStreamWriter(chunk, isappend)
+}
+
+func (ds *dataStore) getStreamWriter(chunk int, isappend bool) (*DataStreamWriter, error) {
+	offset := uint32(0)
+
+	path := ds.genPath(chunk)
+	var fd *os.File
+
+	if stat, err := os.Stat(path); err == nil { // TODO: avoid stat
+		fd, err = os.OpenFile(path, os.O_WRONLY, 0)
+		if err != nil {
+			logger.Infof(err.Error())
+			return nil, err
+		}
+		if isappend {
+			offset = uint32(stat.Size())
+			offset, err := fd.Seek(0, os.SEEK_END)
+			if err != nil {
+				logger.Infof(err.Error())
+				return nil, err
+			} else if offset%256 != 0 {
+				logger.Infof("%s not 256 aligned : %d", path, offset)
+			}
+		}
+	} else {
+		logger.Infof("create data file: %s", path)
+		fd, err = os.Create(path)
+		if err != nil {
+			logger.Infof(err.Error())
+			return nil, err
+		}
+	}
+	wbuf := bufio.NewWriterSize(fd, 1<<20)
+	w := &DataStreamWriter{ds: ds, fd: fd, wbuf: wbuf, offset: offset}
+	return w, nil
+}
---
+++ src/store/datafile.go
@@ -0,0 +1,269 @@
+package store
+
+import (
+	"bufio"
+	"encoding/binary"
+	"fmt"
+	"io"
+	"os"
+)
+
+const (
+	recHeaderSize = 24
+)
+
+var (
+	padding [256]byte
+)
+
+type record struct {
+	rec    *Record
+	crc    uint32
+	ksz    uint32
+	vsz    uint32
+	pos    Position
+	header [recHeaderSize]byte
+}
+
+func newRecord() *record {
+	rec := new(record)
+	rec.rec = new(Record)
+	rec.rec.Payload = new(Payload)
+	return rec
+}
+func wrapRecord(rec *Record) *record {
+	return &record{
+		rec: rec,
+		ksz: uint32(len(rec.Key)),
+		vsz: uint32(len(rec.Payload.ValueCompressed)),
+	}
+}
+
+func (rec *record) decode(data []byte) (err error) {
+	rec.decode(data)
+	decodeHeader(rec, data)
+	rec.rec.Key = data[recHeaderSize : recHeaderSize+rec.ksz]
+	rec.rec.Payload.ValueCompressed = data[recHeaderSize+rec.ksz : recHeaderSize+rec.ksz+rec.vsz]
+	if rec.crc != rec.getHash() {
+		err = fmt.Errorf("crc check fail")
+		logger.Infof(err.Error())
+		return
+	}
+	return nil
+}
+
+func (rec *record) getHash() uint32 {
+	hasher := newCrc32()
+	hasher.write(rec.header[4:])
+	hasher.write(rec.rec.Key)
+	if len(rec.rec.Payload.ValueCompressed) > 0 {
+		hasher.write(rec.rec.Payload.ValueCompressed)
+	}
+	return hasher.get()
+}
+
+func (rec *record) encodeHeader() {
+	h := rec.header[:]
+	binary.LittleEndian.PutUint32(h[4:8], rec.rec.Payload.TS)
+	binary.LittleEndian.PutUint32(h[8:12], rec.rec.Payload.Flag)
+	binary.LittleEndian.PutUint32(h[12:16], uint32(rec.rec.Payload.Ver))
+	binary.LittleEndian.PutUint32(h[16:20], rec.ksz)
+	binary.LittleEndian.PutUint32(h[20:24], rec.vsz)
+	crc := rec.getHash()
+	binary.LittleEndian.PutUint32(h[:4], crc)
+	return
+}
+
+func (rec *record) decodeHeader() (err error) {
+	return decodeHeader(rec, rec.header[:])
+}
+func decodeHeader(rec *record, h []byte) (err error) {
+	rec.crc = binary.LittleEndian.Uint32(h[:4])
+	rec.rec.Payload.TS = binary.LittleEndian.Uint32(h[4:8])
+	rec.rec.Payload.Flag = binary.LittleEndian.Uint32(h[8:12])
+	rec.rec.Payload.Ver = int32(binary.LittleEndian.Uint32(h[12:16]))
+	rec.ksz = binary.LittleEndian.Uint32(h[16:20])
+	rec.vsz = binary.LittleEndian.Uint32(h[20:24])
+	return
+}
+
+func readRecordAtPath(path string, offset uint32) (*record, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		logger.Infof(err.Error())
+		return nil, err
+	}
+	defer f.Close()
+	return readRecordAt(f, offset)
+}
+
+func readRecordAt(f *os.File, offset uint32) (*record, error) {
+	rec := newRecord()
+	if n, err := f.ReadAt(rec.header[:], int64(offset)); err != nil {
+		logger.Infof(err.Error(), n)
+		return nil, err
+	}
+	rec.decodeHeader()
+	kv := make([]byte, rec.ksz+rec.vsz)
+
+	rec.rec.Key = kv[:rec.ksz]
+	rec.rec.Payload.ValueCompressed = kv[rec.ksz:]
+	if n, err := f.ReadAt(kv, int64(offset)+recHeaderSize); err != nil {
+		logger.Infof(err.Error(), n)
+		return nil, err
+	}
+	crc := rec.getHash()
+	if rec.crc != crc {
+		err := fmt.Errorf("crc check fail")
+		logger.Infof(err.Error())
+		return nil, err
+	}
+	return rec, nil
+}
+
+type DataStreamReader struct {
+	ds *dataStore
+
+	fd   *os.File
+	rbuf *bufio.Reader
+
+	chunk  int
+	offset uint32
+}
+
+func newDataStreamReader(path string, bufsz int) (*DataStreamReader, error) {
+	fd, err := os.Open(path)
+	if err != nil {
+		logger.Infof(err.Error())
+		return nil, err
+	}
+	rbuf := bufio.NewReaderSize(fd, bufsz)
+	return &DataStreamReader{fd: fd, rbuf: rbuf, offset: 0}, nil
+}
+
+// TODO: slow
+func (stream *DataStreamReader) nextValid() (r *Record, offset uint32, sizeBroken uint32, err error) {
+	offset2 := stream.offset
+	offset2 = offset2 & (^uint32(255))
+	fd2, _ := os.Open(stream.fd.Name())
+	defer fd2.Close()
+	st, _ := fd2.Stat()
+	for int64(offset) < st.Size() {
+		rec2, err2 := readRecordAt(fd2, offset2)
+		if err2 == nil {
+			logger.Infof("crc fail end %x, sizeBroken %d", offset2, sizeBroken)
+			_, rsize2 := recordSize(rec2.rec)
+			offset3 := offset2 + rsize2
+			stream.fd.Seek(int64(offset3), 0)
+			stream.rbuf.Reset(stream.fd)
+			return rec2.rec, offset2, sizeBroken, nil
+		}
+		sizeBroken += 1
+		offset2 += 256
+	}
+	logger.Infof("crc fail until file end, sizeBroken %d", sizeBroken)
+	return nil, offset2, sizeBroken, nil
+}
+
+func (stream *DataStreamReader) Next() (res *Record, offset uint32, sizeBroken uint32, err error) {
+	rec := newRecord()
+	if _, err = io.ReadFull(stream.rbuf, rec.header[:]); err != nil {
+		if err != io.EOF {
+			logger.Infof(err.Error(), err)
+		} else {
+			err = nil
+		}
+		return
+	}
+	rec.decodeHeader()
+	if rec.ksz > 250 || rec.ksz <= 0 { // TODO
+		logger.Fatalf("bad key len %s %d %d %d", stream.fd.Name(), stream.offset, rec.ksz, rec.vsz)
+		return stream.nextValid()
+	}
+
+	rec.rec.Key = make([]byte, rec.ksz)
+	if _, err = io.ReadFull(stream.rbuf, rec.rec.Key); err != nil {
+		logger.Infof(err.Error())
+		return
+	}
+	rec.rec.Payload.ValueCompressed = make([]byte, rec.vsz)
+	if _, err = io.ReadFull(stream.rbuf, rec.rec.Payload.ValueCompressed); err != nil {
+		logger.Infof(err.Error())
+		return
+	}
+	recsize, _ := recordSize(rec.rec)
+	tail := recsize & 0xff
+	if tail != 0 {
+		stream.rbuf.Discard(int(256 - tail))
+	}
+
+	crc := rec.getHash()
+	if rec.crc != crc {
+		err := fmt.Errorf("crc fail begin offset %x", stream.offset)
+		logger.Infof(err.Error())
+		sizeBroken += 1
+		return stream.nextValid()
+	}
+	res = rec.rec
+	return
+}
+
+func (stream *DataStreamReader) Offset() uint32 {
+	return stream.offset
+}
+
+func (stream *DataStreamReader) Close() error {
+	return stream.fd.Close()
+}
+
+type DataStreamWriter struct {
+	ds   *dataStore
+	fd   *os.File
+	wbuf *bufio.Writer
+
+	chunk  int
+	offset uint32
+}
+
+func (stream *DataStreamWriter) Append(r *Record) (offset uint32, err error) {
+	return stream.offset, stream.append(wrapRecord(r))
+}
+
+func (stream *DataStreamWriter) append(r *record) error {
+	wbuf := stream.wbuf
+	r.encodeHeader()
+	size, sizeall := recordSize(r.rec)
+	stream.offset += sizeall
+	if n, err := wbuf.Write(r.header[:]); err != nil {
+		logger.Infof(err.Error(), n)
+		return err
+	}
+	if n, err := wbuf.Write(r.rec.Key); err != nil {
+		logger.Infof(err.Error(), n)
+		return err
+	}
+	if n, err := wbuf.Write(r.rec.Payload.ValueCompressed); err != nil {
+		logger.Infof(err.Error(), n)
+		return err
+	}
+	npad := sizeall - size
+	if npad != 0 {
+		if n, err := wbuf.Write(padding[:npad]); err != nil {
+			logger.Infof(err.Error(), n)
+			return err
+		}
+	}
+	return nil
+}
+
+func (stream *DataStreamWriter) Offset() uint32 {
+	return stream.offset
+}
+
+func (stream *DataStreamWriter) Close() error {
+	if err := stream.wbuf.Flush(); err != nil {
+		st, _ := stream.fd.Stat()
+		logger.Fatalf("%s %s %s", stream.fd.Name, err.Error(), st.Mode())
+	}
+	return stream.fd.Close()
+}
