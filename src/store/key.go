fa0ce874887d7692d7ed8946c3943dc4c6ec386b
yangxiufeng <yangxiufeng@douban.com>
2015-10-25 10:25:51 +0800

    split key.go from kv.go

---
+++ src/store/key.go
@@ -0,0 +1,130 @@
+package store
+
+import (
+	"fmt"
+	"strconv"
+)
+
+const (
+	MAX_KEY_LEN = 250
+)
+
+func murmur(data []byte) (h uint32) {
+	hasher := murmur3.New32()
+	hasher.Write(data)
+	return hasher.Sum32()
+}
+
+func fnv1a(data []byte) (h uint32) {
+	PRIME := uint32(0x01000193)
+	h = 0x811c9dc5
+	for _, b := range data {
+		h ^= uint32(int8(b))
+		h = (h * PRIME)
+	}
+	return h
+}
+
+func getKeyHash(key []byte) uint64 {
+	return (uint64(fnv1a(key)) << 32) | uint64(murmur(key))
+}
+
+// computed once, before being routed to a bucket
+type KeyPos struct {
+	KeyPathBuf [16]int
+	KeyPath    []int
+
+	// need depth
+	BucketID        int
+	KeyPathInBucket []int
+}
+
+type KeyInfo struct {
+	KeyHash   uint64
+	KeyIsPath bool
+	Key       []byte
+	StringKey string
+	KeyPos
+}
+
+func getBucketFromKey(key string) int {
+	return 0
+}
+
+func ParsePathUint64(khash uint64, buf []int) []int {
+	for i := 0; i < 16; i++ {
+		shift := uint32(4 * (15 - i))
+		idx := int((khash >> shift)) & 0xf
+		buf[i] = int(idx)
+	}
+	return buf
+}
+
+func ParsePathString(pathStr string, buf []int) []int {
+	path := buf[:len(pathStr)]
+	for i := 0; i < len(pathStr); i++ {
+		idx, err := strconv.ParseInt(pathStr[i:i+1], 16, 0)
+		if err != nil {
+			return nil
+		}
+		path[i] = int(idx)
+	}
+	return path
+}
+
+func checkKey(key []byte) error {
+	if len(key) > MAX_KEY_LEN {
+		return fmt.Errorf("key too long: %s", string(key))
+	}
+	return nil
+}
+
+func NewKeyInfoFromBytes(key []byte, keyhash uint64, keyIsPath bool) (ki *KeyInfo) {
+	ki = &KeyInfo{
+		KeyIsPath: keyIsPath,
+		Key:       key,
+		StringKey: string(key),
+		KeyHash:   keyhash,
+	}
+	ki.Prepare()
+	return
+}
+
+func (ki *KeyInfo) getKeyHash() {
+	v := ki.KeyPath
+	shift := uint(60)
+	ki.KeyHash = 0
+	for i := 0; i < len(v); i++ {
+		ki.KeyHash |= (uint64(v[i]) << shift)
+		shift -= 4
+	}
+}
+
+func (ki *KeyInfo) Prepare() {
+	if ki.KeyIsPath {
+		ki.KeyPath = ParsePathString(ki.StringKey, ki.KeyPathBuf[:16])
+		ki.getKeyHash()
+		if len(ki.KeyPath) < config.TreeDepth {
+			ki.BucketID = -1
+			return
+		}
+	} else {
+
+		ki.KeyPath = ParsePathUint64(ki.KeyHash, ki.KeyPathBuf[:16])
+	}
+	for _, v := range ki.KeyPath[:config.TreeDepth] {
+		ki.BucketID <<= 4
+		ki.BucketID += v
+	}
+
+	return
+}
+
+func isSamePath(x, y []int, n int) bool {
+	for i := 0; i < n; i++ {
+		if x[i] != y[i] {
+			return false
+		}
+	}
+	return true
+}
--- src/store/kv.go
+++ src/store/kv.go
@@ -4,38 +4,14 @@ import (
 	"cmem"
 	"fmt"
 	"quicklz"
-	"strconv"
-
-	"github.com/spaolacci/murmur3"
 )
 
 const (
-	MAX_KEY_LEN          = 250
 	FLAG_COMPRESS        = 0x00010000
 	FLAG_CLIENT_COMPRESS = 0x00000010
 	COMPRESS_RATIO_LIMIT = 0.7
 	PADDING              = 256
 )
-
-func murmur(data []byte) (h uint32) {
-	hasher := murmur3.New32()
-	hasher.Write(data)
-	return hasher.Sum32()
-}
-
-func fnv1a(data []byte) (h uint32) {
-	PRIME := uint32(0x01000193)
-	h = 0x811c9dc5
-	for _, b := range data {
-		h ^= uint32(int8(b))
-		h = (h * PRIME)
-	}
-	return h
-}
-
-func getKeyHash(key []byte) uint64 {
-	return (uint64(fnv1a(key)) << 32) | uint64(murmur(key))
-}
 
 type Meta struct {
 	TS        uint32
@@ -192,104 +168,4 @@ func (rec *Record) Size() uint32 {
 
 func (r *Record) Dumps() []byte {
 	return nil
-}
-
-// computed once, before being routed to a bucket
-type KeyPos struct {
-	KeyPathBuf [16]int
-	KeyPath    []int
-
-	// need depth
-	BucketID        int
-	KeyPathInBucket []int
-}
-
-type KeyInfo struct {
-	KeyHash   uint64
-	KeyIsPath bool
-	Key       []byte
-	StringKey string
-	KeyPos
-}
-
-func getBucketFromKey(key string) int {
-	return 0
-}
-
-func ParsePathUint64(khash uint64, buf []int) []int {
-	for i := 0; i < 16; i++ {
-		shift := uint32(4 * (15 - i))
-		idx := int((khash >> shift)) & 0xf
-		buf[i] = int(idx)
-	}
-	return buf
-}
-
-func ParsePathString(pathStr string, buf []int) []int {
-	path := buf[:len(pathStr)]
-	for i := 0; i < len(pathStr); i++ {
-		idx, err := strconv.ParseInt(pathStr[i:i+1], 16, 0)
-		if err != nil {
-			return nil
-		}
-		path[i] = int(idx)
-	}
-	return path
-}
-
-func checkKey(key []byte) error {
-	if len(key) > MAX_KEY_LEN {
-		return fmt.Errorf("key too long: %s", string(key))
-	}
-	return nil
-}
-
-func NewKeyInfoFromBytes(key []byte, keyhash uint64, keyIsPath bool) (ki *KeyInfo) {
-	ki = &KeyInfo{
-		KeyIsPath: keyIsPath,
-		Key:       key,
-		StringKey: string(key),
-		KeyHash:   keyhash,
-	}
-	ki.Prepare()
-	return
-}
-
-func (ki *KeyInfo) getKeyHash() {
-	v := ki.KeyPath
-	shift := uint(60)
-	ki.KeyHash = 0
-	for i := 0; i < len(v); i++ {
-		ki.KeyHash |= (uint64(v[i]) << shift)
-		shift -= 4
-	}
-}
-
-func (ki *KeyInfo) Prepare() {
-	if ki.KeyIsPath {
-		ki.KeyPath = ParsePathString(ki.StringKey, ki.KeyPathBuf[:16])
-		ki.getKeyHash()
-		if len(ki.KeyPath) < config.TreeDepth {
-			ki.BucketID = -1
-			return
-		}
-	} else {
-
-		ki.KeyPath = ParsePathUint64(ki.KeyHash, ki.KeyPathBuf[:16])
-	}
-	for _, v := range ki.KeyPath[:config.TreeDepth] {
-		ki.BucketID <<= 4
-		ki.BucketID += v
-	}
-
-	return
-}
-
-func isSamePath(x, y []int, n int) bool {
-	for i := 0; i < n; i++ {
-		if x[i] != y[i] {
-			return false
-		}
-	}
-	return true
 }
