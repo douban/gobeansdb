edc4601df4b1f4bda1512ddf64a4565cf3d04074
yangxiufeng <yangxiufeng@douban.com>
2016-02-22 12:28:37 +0800

    add config field Cluster

--- config/server_config.go
+++ config/server_config.go
@@ -18,7 +18,8 @@ var (
 
 type ServerConfig struct {
 	Hostname  string `yaml:",omitempty"`
-	ZK        string `yaml:",omitempty"` // e.g. "zk1:2100"
+	Cluster   string `yaml:",omitempty"` // root path in zk
+	ZK        string `yaml:",omitempty"` // e.g. "zk1:2181,zk2:2181"
 	Listen    string `yaml:",omitempty"` // ip
 	Port      int    `yaml:",omitempty"`
 	WebPort   int    `yaml:",omitempty"`
