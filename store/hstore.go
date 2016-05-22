046ffb94fe7ec2793c0a208d624afbadbb202353
SunisDown <isdown.sun@gmail.com>
2016-05-22 14:49:41 +0800

    feat: open bucket concurrent
    并行的执行 bucket.open

--- store/hstore.go
+++ store/hstore.go
@@ -115,14 +115,32 @@ func NewHStore() (store *HStore, err error) {
 	}
 
 	n := 0
+	var wg = sync.WaitGroup{}
+	wg.Add(Conf.NumBucket)
+	errs := make(chan error, Conf.NumBucket)
 	for i := 0; i < Conf.NumBucket; i++ {
-		bkt := store.buckets[i]
-		if Conf.BucketsStat[i] > 0 {
-			err = bkt.open(i, GetBucketPath(i))
-			if err != nil {
-				return
+		go func(id int) {
+			logger.Infof("goroutine why not run %d", id)
+			bkt := store.buckets[id]
+			if Conf.BucketsStat[id] > 0 {
+				err = bkt.open(id, GetBucketPath(id))
+				if err != nil {
+					logger.Errorf("Error in bkt open %s", err.Error())
+					errs <- err
+					//					return
+				} else {
+					n += 1
+				}
 			}
-			n += 1
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+	close(errs)
+	for e := range errs {
+		if e != nil {
+			err = e
+			return
 		}
 	}
 	if Conf.TreeDepth > 0 {
